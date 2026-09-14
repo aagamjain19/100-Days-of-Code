619e18b8d1090f8db6ddfc15bd403722162867ca
aagamjain19 <aagamjain1908@gmail.com>
2026-09-15 01:49:53 +0530

    add

---
+++ Day13Ques1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter operator (+, -, *, /, %%): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    switch (op) {
+
+        case '+':
+            printf("Result = %d", num1 + num2);
+            break;
+
+        case '-':
+            printf("Result = %d", num1 - num2);
+            break;
+
+        case '*':
+            printf("Result = %d", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result = %.2f", (float)num1 / num2);
+            else
+                printf("Cannot divide by zero");
+            break;
+
+        case '%':
+            if (num2 != 0)
+                printf("Result = %d", num1 % num2);
+            else
+                printf("Cannot divide by zero");
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
